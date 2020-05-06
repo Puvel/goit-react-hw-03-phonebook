9e93bc7e27239df95aa0c53eff6caecfa877a6cb
Puvel <puvelko@gmail.com>
2020-05-06 17:28:11 +0300

    Updates

---
+++ precache-manifest.d5c7fb1b8c140c999b8f4287868961ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84eb6dd762711b6c2d5985c792a80d78",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "42792c984c349db5cdd7",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.57558df0.chunk.css"
+  },
+  {
+    "revision": "3dfcf78635881bc7e711",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.1326c8e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.1326c8e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42792c984c349db5cdd7",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.70cbaf0d.chunk.js"
+  },
+  {
+    "revision": "6879e5c8bff0d94ea439",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.675f7826.js"
+  }
+]);
